71e61e1d693d19e2e9147acfc67ae35019a13f51
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-12 16:44:03 -0500

    27- ejercico adivinar numero / 28- condicional ternario

---
+++ ClasesCurso/26-IF.js
@@ -0,0 +1,26 @@
+if (condicion) {
+    //codigo que se ejecuta si la condicion es verdadera
+} else if (condicion2) {
+    //codigo que se ejecuta si la condicion 1 NO es verdadera 
+    //y la condicion 2 SI es verdadera
+} else {
+    //codigo que se ejecuta si ninguna de las condiciones es verdadera 
+}
+
+
+
+if (condicion) {
+    //codigo que se ejecuta si la condicion es verdadera
+} else {
+    //codigo que se ejecuta si la condicion NO es verdadera
+}
+
+
+let name = "Orlando";
+
+if (name === "Jose") {    
+    console.log("Hola, Jose");
+} else if (name === "Orlando"){    
+    console.log("Hola, Orlando")
+} else {
+    console.log("Hola, intruso");}; 
---
+++ ClasesCurso/28-Condicional-ternario.js
@@ -0,0 +1,18 @@
+// let mensaje
+
+// const edad = 20;
+
+// if(edad >= 18){
+//     mensaje = "Es mayor de edad, puede pasar"
+// }else{
+//     mensaje = "No es mayor de edad, no puede pasar"
+// }
+
+// console.log(mensaje)
+
+//ESTRUCTURA CONDICIONAL TERNARIO
+// condicion ? true : false
+
+const edad = 20;
+const mensaje = edad >=18 ? "Es mayor de edad, puede pasar" : "Es menor de edad, no puede pasar"
+console.log(mensaje)
