a40bf6f2966fe04b99b74b487f9761cc761729a1
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-12 17:15:27 -0500

    32-loop-forOf

---
+++ ClasesCurso/32-loop-for of.js
@@ -0,0 +1,15 @@
+/*
+
+for of arrays, strings [algo]  SOLO OBJETOS ITERABLES   
+
+for (variable of objeto) {
+    codigo a ejecutar
+}
+
+*/
+
+let canasta = ["manzana", "pera", "naranja", "mango"]
+
+for ( fruta of canasta){
+    console.log(fruta)
+}
