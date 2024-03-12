c3ea119aaac3fd3ad9700fe8febe72404bf53565
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-12 17:11:17 -0500

    31-loop-forEach

---
+++ ClasesCurso/31-loop-forEach.js
@@ -0,0 +1,13 @@
+/*
+
+array.forEach( (item) => {
+    //código a ejectua
+})
+
+*/
+
+let list = ["eat", "sleep", "code", "play", "repeat"];
+
+list.forEach( (item) => {
+    console.log(item);
+});
