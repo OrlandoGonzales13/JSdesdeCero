02f47d383b0f62b268048a6567e043832cdc0910
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-12 17:07:05 -0500

    30-loop-for

---
+++ ClasesCurso/30-loop-for.js
@@ -0,0 +1,10 @@
+// for(variable; condición; incremento) {
+//     //codigo que se va a ejercutar
+// }
+
+
+let list = ["eat", "sleep", "code", "repeat"];
+
+for(let i = 0; i < list.length ; i++){
+    console.log(list[i])
+}
