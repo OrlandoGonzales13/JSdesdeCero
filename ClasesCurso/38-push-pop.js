5bf8232db80016843ec986f7acfb133c59a5c6a5
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-15 17:53:42 -0500

    38-push-pop

---
+++ ClasesCurso/38-push-pop.js
@@ -0,0 +1,9 @@
+//METODOS QUE MODIFICAN EL ARRAY ORIGINAL (MUTABILITY)
+
+//push()
+
+const paises = [`Argentina`, `Peru`, `USA`]
+const nuevosPaises = paises.push(`Alemania`, `Australia`)
+
+console.log(paises)
+console.log(nuevosPaises)
