51d653333e24b8b3421077ef00e669cf0a9a6c2d
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-03-27 18:22:02 -0500

    44-find and findIndex + EJERCICIO

--- ClasesCurso/45-find-findIndex.js
+++ ClasesCurso/45-find-findIndex.js
@@ -19,3 +19,43 @@ const indexNumber = randomNumber.findIndex(number => number > 50)
 
 console.log(randomNumber) //  [6, 14, 27, 56, 40]
 console.log(indexNumber) // 3
+
+
+// Ganador del sorteo
+// Programa de verificación
+// En este programa podrás verificar si
+// una persona está en la lista de
+// ganadores en un sorteo. Simplemente ingrese
+// el nombre o número de billete, y
+// el programa comprobará y
+// mostrar los ganadores
+
+const winningParticipants=[
+    {id:1,name:'Jennifer',ticketNumber:001},
+    {id:8,name:'Michael',ticketNumber:008}
+    ,{id:15,name:'Emily',ticketNumber:015},
+    {id:47,name:'Charlie',ticketNumber:047}]
+
+function findWinnerByName(name){
+    const winner=winningParticipants.find(participants=>participants.name===name)
+    return winner||'No winner found with that name.'
+}
+
+function findIndexWinnerByTicket(ticketNumber){
+    const index=winningParticipants.findIndex(participants=>participants.ticketNumber===ticketNumber)
+    return index!==-1?index:'No winner found with that ticket number.'
+}
+
+function displayWinnerInformation(winner){
+    if(winner!==undefined&&winner!=null&&winner!=='No winner found with that name.'){
+        console.log('Winner information: ',winner)
+    }else{
+        console.log('No winner found.')
+    }
+}
+
+const winnerByName=findWinnerByName('Emily')
+const indexWinnerByTicket=findIndexWinnerByTicket(015)
+
+displayWinnerInformation(winnerByName)
+console.log('Index of Winner by Ticket Number: ',indexWinnerByTicket)
