ec2e8d00bd14491f3c89d8150b2cef44f23aefcb
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-04-15 17:15:06 -0500

    54-anatomia-de-un-objeto.js

---
+++ ClasesCurso/54-anatomia-de-un-objeto.js
@@ -0,0 +1,26 @@
+/* 
+
+Un objeto es una estructura de datos
+key / value
+
+objeto {
+    propiedad : valor,
+    propiedad : valor,
+    propiedad : valor
+}
+
+*/
+
+const persona = {
+    nombre: "Orlando",
+    edad: 30,
+    direccion: {
+        calle: "Av Insurgente 187",
+        ciudad: "La Perla"
+    },
+    saludar(){
+        console.log(`Hola, mi nombre es ${persona.nombre}`)
+    }
+}
+
+persona.saludar()
